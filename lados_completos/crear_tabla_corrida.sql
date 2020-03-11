5cc5a18c1907a40372bb8959baafcebd19b026df
Hernán Alperin <alpe@alum.mit.edu>
2020-03-11 16:57:49 -0300

    crea tabla corridas

---
+++ lados_completos/crear_tabla_corrida.sql
@@ -0,0 +1,14 @@
+drop table public.corrida;
+CREATE TABLE public.corrida (
+    comando text,
+    prov integer,
+    dpto integer,
+    frac integer,
+    radio integer,
+    conexion text,
+    pwd text,
+    user_host text,
+    cuando date
+);
+
+
