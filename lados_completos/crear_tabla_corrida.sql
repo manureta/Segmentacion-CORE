0758843234e513b51f9ce90aeef3b8811e39693c
manureta <manuel@retamozo.com.ar>
2020-03-22 17:57:29 -0300

    Recupero crear tabla corrida desde 8729870

---
+++ lados_completos/crear_tabla_corrida.sql
@@ -0,0 +1,14 @@
+drop table if exists public.corrida;
+CREATE TABLE public.corrida (
+    comando text,
+    prov integer,
+    dpto integer,
+    frac integer,
+    radio integer,
+    conexion text,
+    pwd text,
+    user_host text,
+    cuando date
+);
+
+
