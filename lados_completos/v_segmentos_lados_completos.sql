0c6d557f1b7e32c5c869931f072e7ee9ecba7a7a
Hernán Alperin <alpe@alum.mit.edu>
2020-10-19 18:11:40 -0300

    vista de segmentos a lados completos con cantidad de viviendas

--- lados_completos/v_segmentos_lados_completos.sql
+++ lados_completos/v_segmentos_lados_completos.sql
@@ -8,20 +8,15 @@ autor: -h
 2020-10-08
 */
 
-create or replace function a_entero(text) returns integer
+create or replace function indec.v_segmentos_lados_completos(esquema text)
+ returns integer
+ language plpgsql volatile
+set client_min_messages = error
 as $function$
-select case
-  when $1 is Null then 0
-  when $1 = '' then 0
-  else $1::integer
-end
-$function$
-language sql immutable
-;
 
-
-drop view e0359.v_segmentos_lados_completos;
-create view e0359.v_segmentos_lados_completos as
+begin
+execute 'drop view if exists "' || esquema || '".v_segmentos_lados_completos';
+execute 'create view "' || esquema || '".v_segmentos_lados_completos as
 with de_e00 as (
     select 
 /*
@@ -30,9 +25,9 @@ with de_e00 as (
       substr(mzai,9,2)::integer as frac, substr(mzai,11,2)::integer as radio,
       substr(mzai,13,3)::integer as mza, 
       ladoi::integer as lado,
-*/    mzai || lpad(ladoi::text,2,'0') as ppdddcccffrrmmmll,
+*/    mzai || lpad(ladoi::text,2,''0'') as ppdddcccffrrmmmll,
       segi as nro_segmento_en_radio
-    from e0359.arc
+    from "' || esquema || '".arc
     where segi is not Null 
     union
     select 
@@ -41,9 +36,9 @@ with de_e00 as (
       substr(mzad,9,2)::integer as frac, substr(mzad,11,2)::integer as radio,
       substr(mzad,13,3)::integer as mza, 
       ladod::integer as lado,
-*/    mzad || lpad(ladod::text,2,'0') as ppdddcccffrrmmmll,
+*/    mzad || lpad(ladod::text,2,''0'') as ppdddcccffrrmmmll,
       segd as nro_segmento_en_radio
-    from e0359.arc
+    from "' || esquema || '".arc
     where segd is not Null
   )
 select ---lados_completos_id serial,
@@ -52,15 +47,29 @@ substr(ppdddcccffrrmmmll,1,12) as ppdddcccffrr,
 nro_segmento_en_radio,
 --array_agg(substr(ppdddcccffrrmmmll,13,5)) as mmmll
 substr(ppdddcccffrrmmmll,13,3)::integer as mza,
-substr(ppdddcccffrrmmmll,16,2)::integer as lado
+substr(ppdddcccffrrmmmll,16,2)::integer as lado,
+conteo as vivs
 from de_e00
+join "' || esquema || '".conteos
+on substr(ppdddcccffrrmmmll,1,2)::integer = prov
+and substr(ppdddcccffrrmmmll,3,3)::integer = dpto
+and substr(ppdddcccffrrmmmll,6,3)::integer = codloc
+and substr(ppdddcccffrrmmmll,9,2)::integer = frac
+and substr(ppdddcccffrrmmmll,11,2)::integer = radio
+and substr(ppdddcccffrrmmmll,13,3)::integer = mza
+and substr(ppdddcccffrrmmmll,16,2)::integer = lado
 order by 
 --prov, dpto, codloc, frac, radio, nro_segmento_en_radio
 substr(ppdddcccffrrmmmll,1,12), nro_segmento_en_radio, mza, lado
 ;
-
-
-select * 
-from e0359.v_segmentos_lados_completos
+'
 ;
+
+return 1;
+end;
+$function$
+;
+
+
+
 
