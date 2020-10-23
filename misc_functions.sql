5eedccb3a7009178f78a7a066404e366320b5d81
Hernán Alperin <alpe@alum.mit.edu>
2020-10-23 08:56:43 -0300

    cerrando pestañas

--- misc_functions.sql
+++ misc_functions.sql
@@ -3,9 +3,12 @@ titulo: misc_functions.sql
 descripción:
 funciones varias para seleccionar segmentos
 a traves de ids y códigos
+
+generalizar foo.sql
+
 autor: -h
 fecha: 2020-10
 */
 
 
-
+create or replace function indec.segmnento(aglomerado text, 
