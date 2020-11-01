72c4ad8c8c8c64a1f79dcbfd1484ab453d646aa8
Hernán Alperin <alpe@alum.mit.edu>
2020-11-01 08:46:26 -0300

    relinea test y agrega length

--- join_ppdddcccffrrmmm.sql
+++ join_ppdddcccffrrmmm.sql
@@ -17,6 +17,7 @@ select
 $1 is not Null 
 and $1 != ''
 and $1 ~ '^[0-9]' -- es numérico
+and length($1) = 15
 and substr($1,1,2)::integer = $2 -- PP = prov
 and substr($1,3,3)::integer = $3 -- DDD = dpto
 and substr($1,6,3)::integer = $4 -- CCC = codloc
@@ -28,14 +29,21 @@ $function$
 
 
 -- Unit tests
-
-select indec.join_ppdddcccffrrmmm(Null,1,2,3,4,5,6) = False as test_null;
-select indec.join_ppdddcccffrrmmm('',1,2,3,4,5,6) = False as test_empty;
-select indec.join_ppdddcccffrrmmm('a',1,2,3,4,5,6) = False as test_no_numeric; -- safe
-select indec.join_ppdddcccffrrmmm('123456789012345',1,2,3,4,5,6) = False as test_no_match;
-select indec.join_ppdddcccffrrmmm('123456789012345',12,345,678,90,12,345) = True as test_match_arbage_anyway;
-select indec.join_ppdddcccffrrmmm('112223334455666',11,222,333,44,55,666) = True as test_match;
-
+with 
+  test_no_null as (select indec.join_ppdddcccffrrmmm(Null,1,2,3,4,5,6) = False as no_null),
+  test_no_empty as (select indec.join_ppdddcccffrrmmm('',1,2,3,4,5,6) = False as no_empty),
+  test_numeric as (select indec.join_ppdddcccffrrmmm('a',1,2,3,4,5,6) = False as is_numeric), -- safe
+  test_no_match as (select indec.join_ppdddcccffrrmmm('123456789012345',1,2,3,4,5,6) = False as no_match),
+  test_match_garbage_anyway as (select indec.join_ppdddcccffrrmmm('123456789012345',12,345,678,90,12,345) = True as match_garbage_anyway),
+  test_match as (select indec.join_ppdddcccffrrmmm('112223334455666',11,222,333,44,55,666) = True as match)
+select *
+from test_no_null
+natural join test_no_empty
+natural join test_numeric
+natural join test_no_match
+natural join test_match_garbage_anyway
+natural join test_match
+;
 
 -- (?) definir qué deben hacer los siguientes tests
 select indec.join_ppdddcccffrrmmm('123456789012',12,345,678,90,12,345) = False as test_no_match_shorter;  
