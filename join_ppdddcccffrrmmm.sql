468be81b20b073ef2372cbe19c53562d4f034a37
Hernán Alperin <alpe@alum.mit.edu>
2020-11-01 05:42:43 -0300

    con unit test pasados

--- join_ppdddcccffrrmmm.sql
+++ join_ppdddcccffrrmmm.sql
@@ -8,9 +8,9 @@ autor: -h
 2020-11
 */
 
-create or replace function indec.join_ppdddcccffrrmmm.sql(PPDDDCCCFFRRMMM text, 
-  prov integer, dpto integer, codloc integer, frac integer. radio integer, mza integer)
-  returns text
+create or replace function indec.join_ppdddcccffrrmmm(PPDDDCCCFFRRMMM text, 
+  prov integer, dpto integer, codloc integer, frac integer, radio integer, mza integer)
+  returns boolean
   language sql immutable
 as $function$
 select
@@ -24,4 +24,15 @@ and substr($1,11,2)::integer = $6 -- RR = radio
 and substr($1,13,3)::integer = $7 -- MMM = mza
 $function$
 ;
+
+
+-- Unit tests
+
+select indec.join_ppdddcccffrrmmm(Null,1,2,3,4,5,6) = False;
+select indec.join_ppdddcccffrrmmm('',1,2,3,4,5,6) = False;
+select indec.join_ppdddcccffrrmmm('123456789012345',1,2,3,4,5,6) = False;
+select indec.join_ppdddcccffrrmmm('123456789012345',12,345,678,90,12,345) = True;
+select indec.join_ppdddcccffrrmmm('112223334455666',11,222,333,44,55,666) = True;
+
+
 
