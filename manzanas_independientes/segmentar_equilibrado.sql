13eb29f5d05d2f338580c130e684f03778659985
Retamozo Manuel <mretamozo@indec.gob.ar>
2020-11-30 16:37:54 -0300

    orden_reco vacio o null, cambio x 0

--- manzanas_independientes/segmentar_equilibrado.sql
+++ manzanas_independientes/segmentar_equilibrado.sql
@@ -33,7 +33,7 @@ listado as (select * from "' || aglomerado || '".listado),
 listado_sin_nulos as (
     select id, prov, dpto, codloc, frac, radio, mza, lado, nrocatastr,
     coalesce(sector,'''') sector, coalesce(edificio,'''') edificio, coalesce(entrada,'''') entrada,
-     piso, coalesce(orden_reco,0)::integer
+     piso, coalesce(CASE WHEN orden_reco='''' THEN NULL ELSE orden_reco END,''0'')::integer orden_reco
     from listado
     ),
 
