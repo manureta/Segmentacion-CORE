0ff7c508c666fe6cf7c8e247b0ee8b90e0bec467
Hernán Alperin <alpe@alum.mit.edu>
2020-12-14 20:34:07 -0300

    queda solo la que elige el segmento

--- descripcion_segmentos/describe_segmentos_con_direcciones.sql
+++ descripcion_segmentos/describe_segmentos_con_direcciones.sql
@@ -10,34 +10,6 @@ fecha: 14/12/2020
 
 */
 
-DROP FUNCTION indec.describe_segmentos_con_direcciones(text);
-create or replace function indec.describe_segmentos_con_direcciones(esquema text)
- returns table (
- prov integer, dpto integer, codloc integer, frac integer, radio integer, 
- mza integer, lado integer, segmento_id bigint, descripcion text
-)
- language plpgsql volatile
-set client_min_messages = error
-as $function$
-begin
-
-return query
-execute '
-with 
-segmento_lado_desde_hasta as (
-    select prov, dpto, codloc, frac, radio, mza, lado, segmento_id, 
-    desde_id, hasta_id,
-    indec.descripcion_calle_desde_hasta(''' || esquema || ''', desde_id, hasta_id)::text as descripcion
-    from "' || esquema || '".segmentos_desde_hasta_ids)
-select prov::integer, dpto::integer, codloc::integer, frac::integer, radio::integer,
-    mza::integer, lado::integer,
-    segmento_id::bigint, descripcion
-from segmento_lado_desde_hasta
-';
-end;
-$function$
-;
-
 DROP FUNCTION indec.describe_segmentos_con_direcciones(text,bigint);
 create or replace function indec.describe_segmentos_con_direcciones(esquema text, s_id bigint)
  returns table (
