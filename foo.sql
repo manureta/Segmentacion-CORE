b9a0da3adf49143f51508bb45ae76ef41637e9be
Hernán Alperin <alpe@alum.mit.edu>
2020-12-16 12:10:05 -0300

    test.sql para corregir bug de segmetnar_equilibrado

--- foo.sql
+++ foo.sql
@@ -70,6 +70,82 @@ where orden_reco = '' or orden_rec2 = ''
 
 
 
+select id, orden_reco, mza, lado, segmento_id, nrocatastr
+from e0002.segmentacion
+join e0002.listado
+on listado.id = listado_id
+where segmento_id = 25
+order by mza, lado, id, orden_reco
+;
+
+select *
+from e0002.segmentos_desde_hasta_ids
+where segmento_id = 25
+;
+
+select *
+from e0002.segmentos_desde_hasta
+where segmento_id = 25
+;
+                                                                                                                                                                       
+with
+parametros as (select 36::float as deseado),
+listado as (select * from e0002.listado),
+listado_sin_nulos as (
+    select id, prov, dpto, codloc, frac, radio, mza, lado, nrocatastr,
+    coalesce(sector,'') sector, coalesce(edificio,'') edificio, coalesce(entrada,'') entrada,
+    piso, coalesce(CASE WHEN orden_reco='' THEN NULL ELSE orden_reco END,'0')::integer orden_reco
+    from listado
+    ),
+casos as (
+    select prov, dpto, codloc, frac, radio, mza,
+           count(*) as vivs, ceil(count(*)/deseado) as max, greatest(1, floor(count(*)/deseado)) as min
+    from listado_sin_nulos, parametros
+    group by prov, dpto, codloc, frac, radio, mza, deseado
+    order by prov, dpto, codloc, frac, radio, mza, deseado
+    ),
+deseado_manzana as (
+    select prov, dpto, codloc, frac, radio, mza, vivs,
+        case when abs(vivs/max - deseado) < abs(vivs/min - deseado) then max else min end as segs_x_mza
+    from casos, parametros
+    ),
+pisos_enteros as (
+    select prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, min(id) as piso_id
+    from listado_sin_nulos
+    group by prov, dpto, codloc, frac, radio, mza, lado,
+        nrocatastr, sector, edificio, entrada, piso
+    ),
+pisos_abiertos as (
+    select id, prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, orden_reco::integer, piso_id,
+        row_number() over w as row, rank() over w as rank
+    from pisos_enteros
+    natural join listado_sin_nulos
+    window w as (
+        partition by prov, dpto, codloc, frac, radio, mza
+        order by lado::integer, orden_reco::integer)
+    ),
+segmento_id_en_mza as (
+    select id, piso_id, prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, orden_reco::integer,
+        floor((rank - 1)*segs_x_mza/vivs) + 1 as sgm_mza, rank
+    from deseado_manzana
+    join pisos_abiertos
+    using (prov, dpto, codloc, frac, radio, mza)
+    )
+select prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, sgm_mza, count(*)
+from segmento_id_en_mza
+group by prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, sgm_mza
+;
 
 
+
+segmentos_id as (
+    select
+        -- row_number() over (order by dpto, frac, radio, mza, sgm_mza)
+        nextval(''"' || aglomerado || '".segmentos_seq'')
+        as segmento_id,
+        dpto, frac, radio, mza, sgm_mza
+    from segmento_id_en_mza
+    group by dpto, frac, radio, mza, sgm_mza
+    order by dpto, frac, radio, mza, sgm_mza
+    )
 
---
+++ test.sql
@@ -0,0 +1,61 @@
+with
+parametros as (select 36::float as deseado),
+listado as (select * from e0002.listado),
+listado_sin_nulos as (
+    select id, prov, dpto, codloc, frac, radio, mza, lado, nrocatastr,
+    coalesce(sector,'') sector, coalesce(edificio,'') edificio, coalesce(entrada,'') entrada,
+    piso, coalesce(CASE WHEN orden_reco='' THEN NULL ELSE orden_reco END,'0')::integer orden_reco
+    from listado
+    ),
+casos as (
+    select prov, dpto, codloc, frac, radio, mza,
+           count(*) as vivs, ceil(count(*)/deseado) as max, greatest(1, floor(count(*)/deseado)) as min
+    from listado_sin_nulos, parametros
+    group by prov, dpto, codloc, frac, radio, mza, deseado
+    order by prov, dpto, codloc, frac, radio, mza, deseado
+    ),
+deseado_manzana as (
+    select prov, dpto, codloc, frac, radio, mza, vivs,
+        case when abs(vivs/max - deseado) < abs(vivs/min - deseado) then max else min end as segs_x_mza
+    from casos, parametros
+    ),
+pisos_enteros as (
+    select prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, min(id) as piso_id
+    from listado_sin_nulos
+    group by prov, dpto, codloc, frac, radio, mza, lado,
+        nrocatastr, sector, edificio, entrada, piso
+    ),
+pisos_abiertos as (
+    select id, prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, orden_reco::integer, piso_id,
+        row_number() over w as row, rank() over w as rank
+    from pisos_enteros
+    natural join listado_sin_nulos
+    window w as (
+        partition by prov, dpto, codloc, frac, radio, mza
+        order by lado::integer, orden_reco::integer)
+    ),
+particion as (
+    select id, piso_id, prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, orden_reco::integer,
+        floor((rank - 1)*segs_x_mza/vivs) + 1 as sgm_mza, rank
+    from deseado_manzana
+    join pisos_abiertos
+    using (prov, dpto, codloc, frac, radio, mza)
+    ),
+particion_piso_sgm as (
+  select prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, sgm_mza
+  from particion
+  group by prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, sgm_mza
+  ),
+pisos_partidos as (
+  select prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso
+  from particion_piso_sgm
+  group by prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso
+  having count(*) > 1
+  )
+select prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, sgm_mza, count(*)
+from particion
+natural join pisos_partidos
+group by prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, sgm_mza
+order by prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, sgm_mza
+;
+
