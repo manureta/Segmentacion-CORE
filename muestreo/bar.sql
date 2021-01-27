f0d77c8e114c075784cf0513245e17aed8f75fa2
Hernán Alperin <alpe@alum.mit.edu>
2021-01-27 16:55:03 -0300

    el select parte en dos

--- muestreo/bar.sql
+++ muestreo/bar.sql
@@ -8,7 +8,7 @@ with a_partir as (
   on segmento_id = s_id
   where muestreado
   ),
-  carga_segmentos as (
+carga_segmentos as (
   select prov, dpto, codloc, frac, radio, s_id, count(*) as cantidad
   from a_partir
   group by prov, dpto, codloc, frac, radio, s_id
@@ -38,24 +38,51 @@ asignacion_segmentos_pisos_enteros as (
     from asignacion_segmentos
     group by prov, dpto, codloc, frac, radio, mza, lado,
         nrocatastr, sector, edificio, entrada, piso, s_id
+    ),
+
+asignacion_sin_cortar_piso as (
+    select prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso, p.sgm_listado, p.s_id, orden_reco
+    from asignacion_segmentos_pisos_enteros p
+    join asignacion_segmentos
+    using (prov, dpto, codloc, frac, radio, mza, lado, nrocatastr, sector, edificio, entrada, piso)
     )
 
-select *
-from a_partir
+
+
+select *, case
+  when sgm_listado = 1 then pre_censal_id1
+  when sgm_listado = 2 then pre_censal_id2
+  else 0 -- ERROR
+  end
+from e0002.muestra
+join asignacion_sin_cortar_piso
+on s_id = pos_censal_id
+where frac::integer = 14 and radio::integer = 9
+order by orden_reco::integer
+
+
+;
 
 /*
-update e0002.segmentacion sgm
+update e0002.segmentacion_pos_muestra sgm
 set segmento_id = case
   when sgm_listado = 1 then pre_censal_id1
   when sgm_listado = 2 then pre_censal_id2
   else 0 -- ERROR
   end
 from (e0002.muestra
-join asignacion_segmentos_pisos_enteros
+join asignacion_sin_cortar_piso
 on s_id = pos_censal_id) j
 where s_id = pos_censal_id
-
-*/
-
 ;
 
+
+
+select segmento_id, count(*)
+from e0002.segmentacion_pos_muestra
+join e0002.listado
+on listado.id = listado_id
+where frac::integer = 14 and radio::integer = 9
+group by segmento_id
+;
+*/
