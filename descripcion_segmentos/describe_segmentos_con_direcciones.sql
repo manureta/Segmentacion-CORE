9e8f51428561c55982ab3bd5aebc423cb438441e
Hernán Alperin <alpe@alum.mit.edu>
2021-01-29 17:31:47 -0300

    = cáscara describe_segmentos_con_direcciones no usa tabla intermedia esquema.segmendo_lado_desde_hasta_ids REEMPLAZA A LA ANTERIOR QUE USABA TABLAS INTERMEDIAS segmentos_desde_hasta_ids

--- descripcion_segmentos/describe_segmentos_con_direcciones.sql
+++ descripcion_segmentos/describe_segmentos_con_direcciones.sql
@@ -3,16 +3,15 @@ titulo: describe_segmentos_con_direcciones.sql
 descripción:
 crea la descripcion de un dado segmento, usando mzas, lados, y desde hasta por lados
 para agregarse en el mapa del radio
-
-
+no usa tabla intermedia esquema.segmendo_lado_desde_hasta_ids
+REEMPLAZA A LA ANTERIOR QUE USABA TABLAS INTERMEDIAS segmentos_desde_hasta_ids
 autor: -h
-fecha: 14/12/2020
+fecha: 2021-01-29
 
 */
 
-
-DROP FUNCTION if exists indec.describe_segmentos_con_direcciones(text);
-create or replace function indec.describe_segmentos_con_direcciones(esquema text)
+DROP FUNCTION if exists indec.describe_segmentos_con_direccionesi_ff(text);
+create or replace function indec.describe_segmentos_con_direcciones_ff(esquema text)
  returns table (
  prov integer, dpto integer, codloc integer, frac integer, radio integer,
  segmento_id bigint, seg text, descripcion text, viviendas numeric
@@ -24,61 +23,110 @@ begin
 
 return query
 execute '
-with minimos as (
-  select prov, dpto, codloc, frac, radio, segmento_id, 
-    min(mza::integer) as minmza, min(lado::integer) as minlado, min(
-    (CASE WHEN orden_reco='''' THEN 0 ELSE orden_reco::integer END)::integer) as minreco, 
-    min(sector) as minsector, min(edificio) as minedificio, min(entrada) as minentrada,
-    max(REGEXP_REPLACE(COALESCE(piso::character varying, ''0''), ''[^0-9]*'' ,''0'')::integer) as maxpiso
+with
+  listado as ( select id, prov, dpto, codloc, frac, radio, mza, lado,
+    coalesce(CASE WHEN orden_reco='''' THEN NULL ELSE orden_reco END,''0'')::integer orden_reco,
+    tipoviv, sector, edificio, entrada, piso
   from "' || esquema || '".listado
-  join "' || esquema || '".segmentacion
+  ),
+  segmentacion as ( select * from "' || esquema || '".segmentacion),
+  segmentos_row_number as (
+  select prov, dpto, codloc, frac, radio, segmento_id, mza::integer as mza, lado::integer lado, 
+    orden_reco::integer orden_reco, sector, edificio, entrada,
+    REGEXP_REPLACE(COALESCE(piso::character varying, ''0''), ''[^0-9]*'' ,''0'')::integer as piso,
+    row_number () over (
+      partition by prov, dpto, codloc, frac, radio, segmento_id
+      order by mza::integer, lado::integer, orden_reco::integer, sector, edificio, entrada,
+      REGEXP_REPLACE(COALESCE(piso::character varying, ''0''), ''[^0-9]*'' ,''0'')::integer desc
+      ) as rnk
+  from listado
+  join segmentacion
   on listado_id = listado.id
-  group by prov, dpto, codloc, frac, radio, segmento_id
+  group by prov, dpto, codloc, frac, radio, segmento_id, mza, lado, orden_reco, sector, edificio, entrada, piso
   ),
 etiquetas as (
   select segmento_id, rank() over w as seg
-  from minimos
+  from segmentos_row_number
+  where rnk = 1
   window w as (
     partition by prov, dpto, codloc, frac, radio
-    order by minmza, minlado, minreco, minsector, minedificio, minentrada, maxpiso desc
+    order by mza, lado, orden_reco, sector, edificio, entrada, piso desc
     )
   ),
-segmento_lado_desde_hasta as (
+
+lados_desde_hasta as ( -- desde hasta por lado
+  select prov, dpto, codloc, frac, radio, mza, lado,
+    min(listado.orden_reco) as lado_desde, max(listado.orden_reco) as lado_hasta
+  from listado
+  group by prov, dpto, codloc, frac, radio, mza, lado
+  ),
+segmentos_lados_desde_hasta as ( -- desde hasta lado por segmento
+  select prov, dpto, codloc, frac, radio, mza, lado, segmento_id,
+    min(listado.orden_reco) as seg_lado_desde, max(listado.orden_reco) as seg_lado_hasta,
+    (min(listado.orden_reco) = lado_desde and max(listado.orden_reco) = lado_hasta) as completo,
+    count(indec.contar_vivienda(tipoviv)) as viviendas
+  from listado join segmentacion on listado.id = listado_id
+  join lados_desde_hasta
+  using (prov, dpto, codloc, frac, radio, mza, lado)
+  group by prov, dpto, codloc, frac, radio, mza, lado, segmento_id, lado_desde, lado_hasta
+  ),
+segmentos_lados_desde_hasta_ids as (
+  select prov, dpto, codloc, frac, radio, mza, lado, segmento_id, 
+    desde_id, hasta_id, seg_lado_desde, seg_lado_hasta,
+    completo, viviendas
+  from (
+    select l.prov, l.dpto, l.codloc, l.frac, l.radio, l.mza, l.lado, s.segmento_id, l.id as desde_id,
+    seg_lado_desde, viviendas, completo
+    from segmentos_lados_desde_hasta s
+    join listado l on s.prov = l.prov and s.dpto = l.dpto and s.codloc = l.codloc
+    and s.frac = l.frac and s.radio = l.radio and s.mza = l.mza and s.lado = l.lado
+    and seg_lado_desde = orden_reco
+    ) as desdes
+    natural join (
+    select l.prov, l.dpto, l.codloc, l.frac, l.radio, l.mza, l.lado, s.segmento_id, l.id as hasta_id,
+    seg_lado_hasta, viviendas, completo
+    from segmentos_lados_desde_hasta s
+    join listado l on s.prov = l.prov and s.dpto = l.dpto and s.codloc = l.codloc
+    and s.frac = l.frac and s.radio = l.radio and s.mza = l.mza and s.lado = l.lado
+    and seg_lado_hasta = orden_reco
+    ) as hastas
+  ),
+
+segmentos_descripcion_lado as (
   select prov, dpto, codloc, frac, radio, mza, lado, segmento_id,
     desde_id, hasta_id, completo,
     ''Lado '' || lpad(lado::integer::text, 2, ''0'') ||
-      case when completo then '' completo ''
-           else '' ''
-      end ||
+      case when completo then '' completo '' else '' '' end ||
     indec.descripcion_calle_desde_hasta(''' || esquema || ''', desde_id, hasta_id, completo)::text as descripcion,
     viviendas
-  from "' || esquema || '".segmentos_desde_hasta_ids
+  from segmentos_lados_desde_hasta_ids
   ),
-segmento_lados as (
+segmentos_descripcion_mza as (
   select prov::integer, dpto::integer, codloc::integer, frac::integer, radio::integer,
-    mza::integer, segmento_id::bigint, 
+    mza::integer, segmento_id::bigint,
     string_agg(descripcion, '', '') as descripcion,
     sum(viviendas) as viviendas
-  from segmento_lado_desde_hasta
+  from segmentos_descripcion_lado
   group by prov::integer, dpto::integer, codloc::integer, frac::integer, radio::integer,
     mza::integer, segmento_id::bigint
   order by prov::integer, dpto::integer, codloc::integer, frac::integer, radio::integer,
     mza::integer, segmento_id::bigint
-
   )
+
 select prov::integer, dpto::integer, codloc::integer, frac::integer, radio::integer,
-  segmento_id::bigint, seg::text, 
+  segmento_id::bigint, seg::text,
   string_agg(''Manzana '' || lpad(mza::integer::text, 3, ''0'') || '': '' || descripcion, ''. '') as descripcion,
   sum(viviendas) as viviendas
-from segmento_lados
-join etiquetas
+from segmentos_descripcion_mza
+join etiquetas 
 using (segmento_id)
 group by prov::integer, dpto::integer, codloc::integer, frac::integer, radio::integer,
   segmento_id::bigint, seg::text
-';
+order by seg::text
+
+;';
 end;
 $function$
 ;
-
 
 
